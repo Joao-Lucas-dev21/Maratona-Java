6fc52b11d6ce8109eaca5faae010fef59b3b7d90
Joao-Lucas-dev21 <joaolucas2004-o@hotmail.com>
2025-04-15 15:28:30 -0300

    docs: Adicionando mais arquivos de estudos

---
+++ src/Exercicios/dominio/Seminario.java
@@ -0,0 +1,11 @@
+package Exercicios.dominio;
+
+// Crie um sistema que gerêncie seminários
+// O sistema deverá cadastrar seminários, estudantes, professores, e local onde será realizado
+// Um aluno poderá estar em apenas um seminário
+// Um seminário poderá ter nenhum ou vários alunos
+/
+//
+
+public class Seminario {
+}
