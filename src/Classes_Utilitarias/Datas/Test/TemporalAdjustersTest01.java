77ceac1c2c24522380197eb8bc6adf1e149e68f1
Joao-Lucas-dev21 <joaolucas2004-o@hotmail.com>
2025-05-08 15:07:52 -0300

    TemporalAdjusters

---
+++ src/Classes_Utilitarias/Datas/Test/TemporalAdjustersTest01.java
@@ -0,0 +1,45 @@
+package Classes_Utilitarias.Datas.Test;
+
+import Orientacao_Objetoss.Associacao.dominio.Local;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+
+public class TemporalAdjustersTest01 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        now = now.withDayOfMonth(8);//Set o dia, mas tem o mes e ano
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.nextOrSame(DayOfWeek.THURSDAY));//Retorna a data
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.THURSDAY));//Retorna na semana o proximo dia
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.previous(DayOfWeek.THURSDAY));//Retorna o dia anterior na semana
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.firstDayOfMonth());//Retorna primeiro dia do mês
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.lastDayOfMonth());//Retorna ultimo dia do mês
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.firstDayOfNextYear());//Retorna primeiro dia do ano que vem
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.firstDayOfNextMonth());//Retorna primeiro dia do mes que vem
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+    }
+}
